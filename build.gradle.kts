378ce2ed58d473c3e159a7883734defdda76fd89
Maximov Valery <maximovvalery@gmail.com>
2026-02-23 08:50:44 +0300

    Обновление зависимостей и JDK21 (#65)
    * обновление зависимостей
    
    * jdk21
    
    * api

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,15 +48,17 @@ gitVersioning.apply {
 }
 
 dependencies {
-    implementation("io.github.1c-syntax", "antlr4", "0.3.0-rc.2")
-    implementation("commons-io", "commons-io", "2.15.1")
-    implementation("org.junit.jupiter", "junit-jupiter-api", "5.11.4")
-    implementation("org.assertj", "assertj-core", "3.27.0")
+    implementation("io.github.1c-syntax:antlr4:0.3.0")
 
-    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.11.4")
-    testImplementation("org.mockito", "mockito-core", "5.21.0")
+    implementation("commons-io:commons-io:2.21.0")
+    api("org.junit.jupiter:junit-jupiter-api:6.0.3")
+    api("org.assertj:assertj-core:3.27.7")
 
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.4")
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.mockito:mockito-core:5.21.0")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<JavaCompile> {
@@ -65,8 +67,8 @@ tasks.withType<JavaCompile> {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
     withSourcesJar()
     withJavadocJar()
 }
